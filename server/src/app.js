bbfd3ee84064ddf3196ef741109a4e16680eb08a
anuujj <anujk8097@gmail.com>
2024-06-06 22:24:32 +0530

    cors fix and request fix

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "https://voyager-anuujj.vercel.app/",
+  origin: "https://voyager-anuujj.vercel.app",
   optionsSuccessStatus: 200,
 };
 
