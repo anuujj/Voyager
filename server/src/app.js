a6c88da14272a67659dac9f7e7b73f15dbe776d8
anuujj <anujk8097@gmail.com>
2024-06-07 00:09:46 +0530

    fixed the url

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const allowedOrigins = [
-  "https://voyager-anuujj.vercel.app/",
+  "https://voyager-anuujj.vercel.app",
   "http://localhost:5173",
   "http://localhost:5174",
   // add more origins as needed
